ead38315b5b9645c49b49ed9282a0c3192f66abd
Janet Garcia <janetgarcia1989@gmail.com>
2024-05-16 16:31:30 -0300

    2:50:52 Formulario SignUp

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session');
+const passport = require('passport');
 
 const { database } = require('./keys');
 
 //Initializations
 const app = express();
+require('./lib/passport');
 
 //Settings
 app.set('port', process.env.PORT || 4000);
@@ -35,7 +37,8 @@ app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 //Global Variables 
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failuerFlash: true
+}))
+
+router.get('/profile', (req, res) => {
+    res.send('this is Profile')
+});
+
 module.exports = router;
